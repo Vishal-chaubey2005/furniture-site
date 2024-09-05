389f7a08538603c0b64ee6aeabb5c0064b25886f
Vishal-chaubey2005 <imvishalchaubey@gmail.com>
2024-09-05 14:35:08 +0530

    Update Navbar.jsx

--- furniture fronted/src/Components/Navbar.jsx
+++ furniture fronted/src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar({ one }) {
       >
         <div className="container">
           <Link className="navbar-brand" to="/">
-            Furni<span>.</span>
+            VishalChaubey<span>.</span>
           </Link>
           <button
             className="navbar-toggler"
